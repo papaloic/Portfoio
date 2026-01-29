0d03be0e9edcf7947e2a94531726e920951d4ab3
loic <papaloic@gmail.com>
2026-01-29 20:22:27 +0100

    maintenance 1

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,10 +13,10 @@ const hero = () => {
   return (
     <section class="pub w-full h-auto flex flex-col gap-10 md:gap-5 items-center justify-center md:mt-[10vh] bg-white p-2 text-center md:p-0">
       <h1 data-aos="zoom-out-down" class="sect-h1 text-black mt-10">Rejoignez la communauté</h1>
-      <div class="pub_b w-full md:h-[60vh] flex flex-col md:flex-row md:mt-[8vh]">
-        <div data-aos="fade-right" class="pub_gauche w-full md:w-1/2 flex justify-center items-center relative pl-8">
-          <img src={hero_G} alt="" class="w-full h-auto object-cover opacity-30" />
-          <p className=' absolute m-auto text-black font-semibold text-xl md:text-start italic'>J’aime concevoir des applications fiables, apprendre de nouvelles technologies et transformer des idées en projets concrets. Curieux et rigoureux, je m’intéresse autant au développement front-end qu’au back-end, avec une attention particulière portée à la qualité du code et à la sécurité des services <br /><br />
+      <div class="pub_b w-full h-auto md:h-[60vh] flex flex-col md:flex-row md:mt-[8vh]">
+        <div data-aos="fade-right" class="pub_gauche w-full h-auto md:w-1/2 flex justify-center items-center relative md:pl-8">
+          <img src={hero_G} alt="" class="w-full h-[55vh] md:h-auto object-contain opacity-30" />
+          <p className=' absolute m-auto text-black font-semibold text-xl italic'>J’aime concevoir des applications fiables, apprendre de nouvelles technologies et transformer des idées en projets concrets. Curieux et rigoureux, je m’intéresse autant au développement front-end qu’au back-end, avec une attention particulière portée à la qualité du code et à la sécurité des services <br /><br />
           Actuellement en formation, je développe continuellement mes compétences à travers des projets personnels et académiques</p>
         </div>
         <div data-aos="fade-left" class="pub_droite w-full md:w-1/2 p-2 md:p-10 flex flex-wrap justify-center items-center gap-[20%]">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
     }, []);
     return (
         <nav data-aos="fade-down" className="nav w-full h-[10vh] md:h-[15vh] top-0 fixed bg-white flex justify-between p-5 items-center shadow-xl/20 z-20">
-            <div data-aos="fade-right" className="logo flex items-center w-[60%] md:w-[30%] md:ml-[5%]">
+            <div data-aos="fade-right" className="logo flex gap-0.5 items-center w-[60%] md:w-[30%] md:ml-[5%]">
                 <div className="dropdown md:hidden w-[18%]">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+                <div tabIndex={0} role="button" className="btn btn-primary btn-ghost btn-circle size-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current btn-primary"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
                 </div>
                 <ul
                     tabIndex="-1"
-                    className="menu_link_phone menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow ">
+                    className="menu_link_phone menu menu-sm dropdown-content bg-neutral-100 rounded-box z-1 mt-3 w-52 p-2 shadow ">
                     <li><a href="#acceuil">Acceuil</a></li>
                     <li><a href="#about">A propos</a></li>
                     <li><a href="#competences">Compétences</a></li>
